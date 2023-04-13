69229cd4a48731af903cb5372df3a06c9c2d59c7
NILOY ROY <121684663+xanon-oc@users.noreply.github.com>
2023-04-13 22:37:43 +0600

    firebase integration

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,22 +1,41 @@
 import React, { useState } from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "../../firebase/firebase.config";
+
+const auth = getAuth(app);
 
 const Register = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  // const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setSuccess("");
     const email = event.target.email.value;
     const password = event.target.password.value;
-    console.log(email, password);
+
+    // Create user in firebase
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        setError("");
+        event.target.reset("");
+        setSuccess("User has created account successfully");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
   };
 
   const handleEmailChange = (event) => {
-    console.log(event.target.value);
-    setEmail(event.target.value);
+    // console.log(event.target.value);
+    // setEmail(event.target.value);
   };
   const handlePasswordBlur = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
   };
   return (
     <div>
@@ -26,18 +45,23 @@ const Register = () => {
           onChange={handleEmailChange}
           type="email"
           name="email"
+          required
           id="email"
           placeholder="Enter your email"
         />
         <br />
+
         <input
           onBlur={handlePasswordBlur}
           type="password"
           name="password"
+          required
           id="password"
           placeholder="Your password"
         />
         <br />
+        <p className="text-danger">{error}</p>
+        <p className="text-success">{success}</p>
         <input type="submit" value="Register" />
       </form>
     </div>
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Fb1KyfbHFiXTM7xDRklYpsLxHIDJZbc",
+  authDomain: "email-password-auth-74dc4.firebaseapp.com",
+  projectId: "email-password-auth-74dc4",
+  storageBucket: "email-password-auth-74dc4.appspot.com",
+  messagingSenderId: "265564999670",
+  appId: "1:265564999670:web:1c71a2d51daed4a33541d7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
